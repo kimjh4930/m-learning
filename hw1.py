baa193f27c524434f20a257b6e2c80409ef8b7d5
Junha <kimjh4930@gmail.com>
2015-10-13 23:11:24 -0700

    before format

---
+++ hw1.py
@@ -0,0 +1,5 @@
+from sklearn.datasets import fetch_20newsgroups
+from pprint import pprint
+
+newsgroup_train = fetch_20newsgroups(subset='train')
+pprint(list(newsgroups_train.target_names))
---
+++ search.py
@@ -0,0 +1,5 @@
+import glob
+
+file_list = glob.glob("/home/junha/m-learning/20_newsgroups/comp.graphics/*")
+
+print file_list
